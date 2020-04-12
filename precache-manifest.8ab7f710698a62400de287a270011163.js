41b39e38be59c8463d69b14dfaf841ddfbe32246
Loremar Marabillas <loremar5858@gmai.com>
2020-04-12 17:25:12 +0800

    Updates

---
+++ precache-manifest.8ab7f710698a62400de287a270011163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e60e8531dd96ac9edb5c1a3713b667",
+    "url": "/FCC-random-quote-machine/index.html"
+  },
+  {
+    "revision": "75b9a31f4cf2d6f4eb27",
+    "url": "/FCC-random-quote-machine/static/css/main.7897b8de.chunk.css"
+  },
+  {
+    "revision": "3b66cc1a242478500af9",
+    "url": "/FCC-random-quote-machine/static/js/2.311a54c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FCC-random-quote-machine/static/js/2.311a54c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b9a31f4cf2d6f4eb27",
+    "url": "/FCC-random-quote-machine/static/js/main.3873e0ff.chunk.js"
+  },
+  {
+    "revision": "ed36751013d3120aa58a",
+    "url": "/FCC-random-quote-machine/static/js/runtime-main.05b8e297.js"
+  }
+]);
